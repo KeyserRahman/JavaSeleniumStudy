32ffadf5fd0be9768aed389b03eaf0f07bc7b5ec
KeyserRahman <reheman0201@gmail.com>
2022-09-20 22:19:28 -0700

    created Shape package and abstract classes

---
+++ src/test/java/Shape/ShapeObjects.java
@@ -0,0 +1,12 @@
+package Shape;
+
+public class ShapeObjects {
+    public static void main(String[] args) {
+        //Shape shape = new Shape();
+        Circle circle = new Circle();
+        Rectangle rectangle = new Rectangle();
+        Square square = new Square();
+        System.out.println(circle.area());
+
+    }
+}
